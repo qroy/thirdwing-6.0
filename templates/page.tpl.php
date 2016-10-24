76970ab0230d210697d098afe056eb8b33723dc8
Roy Veldman <roy@vldmn.com>
2016-10-24 14:01:51 +0200

    Base 3.0 branch

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -5,15 +5,17 @@
   $time = format_date($node->changed, 'custom', 'H:m');
 
   if ($node->changed):
-  $last_updated = '<strong>Pagina laatst aangepast op ' . $date . ' om ' . $time . '</strong><br /><br />';
+  $last_updated = '<p>Pagina laatst aangepast op ' . $date . ' om ' . $time . '</p>';
   endif;
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
-  <head>
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
+  "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
+<html lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
+<head profile="http://www.w3.org/1999/xhtml/vocab">
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php print $head ?>
     <title><?php print $head_title ?></title>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <?php print $styles ?>
 
     <!--[if lt IE 7]>
@@ -34,57 +36,58 @@
   </head>
   <body class="<?php print $body_classes; ?>">
 
-<!-- Layout -->
-    <div id="wrapper">
-    <div id="container" class="clear-block">
-<div id="topbar">
-      <?php print $topbar; ?>
+<div id="top"></div>
 
+<div id="wrapper">
+
+  <div class="container clear-block"><div id="topbar"><?php print $topbar; ?></div></div>
+
+  <div class="container clear-block">
+  <div id="logo">
+      <div id="logo-img"></div>
+      <div id="logo-title"><?php print $site_name; ?></div>
+  </div> <!-- /logo -->
+  </div>
+
+  <div class="clear-block container">
+      
+    <div id="header">
+      <nav id="nav" role="navigation"><a href="#" class="toggle-nav icon icon-bars">Menu</a><?php print $header; ?></nav>
+    </div> <!-- /header -->
+
+    <?php if ($left): ?><div id="sidebar-left" class="sidebar"><?php print $left ?></div><?php endif; ?>
+
+    <div id="center"><div id="squeeze">
+      <div id="breadcrumb"><?php print $breadcrumb; ?></div>
+      <?php if ($tabs): print '<div id="tabs-wrapper" class="clear-block"><a href="#" class="toggle-tabs icon icon-pencil-square-o">Bewerk</a><ul class="tabs primary">'. $tabs;
+            if (arg(0) == 'node') :
+            global $user; $user_roles = array_keys($user->roles);
+            if (in_array(6, $user_roles) || in_array(12, $user_roles)) { print '<li><a href="' . $_GET['q'] . '">' . $_GET['q'] . '</a></li>'; }
+            endif;
+            print '</ul></div>'; endif; 
+      ?>
+      <?php if ($tabs2): print '<ul class="tabs secondary">'. $tabs2 .'</ul>'; endif; ?>
+      <?php if ($show_messages && $messages): print $messages; endif; ?>
+      <?php print $help; ?>
+      <?php if ($title): print '<h1>'. $title .'</h1>'; endif; ?>
+      <div class="clear-block">
+      <?php print $content ?>
       </div>
-      <div id="header">
-      <nav id="nav" role="navigation"><?php print $header; ?></nav>
-      </div> <!-- /header -->
+      <?php print $feed_icons ?>
+    </div></div> <!-- /#squeeze, /#center -->
 
-      <?php if ($left): ?>
-        <div id="sidebar-left" class="sidebar">
-          <?php if ($search_box): ?><div class="block block-theme"><?php print $search_box ?></div><?php endif; ?>
-          <?php print $left ?>
-        </div>
-      <?php endif; ?>
+    <?php if ($right): ?><div id="sidebar-right" class="sidebar"><?php print $right ?></div><?php endif; ?>
 
-      <div id="center"><div id="squeeze"><div class="right-corner"><div class="left-corner">
-          <?php print $breadcrumb; ?>
-          <?php if ($tabs): print '<div id="tabs"><ul class="tabs primary">'. $tabs .'</ul>'; endif; ?>
-          <?php if ($tabs): print '</div>'; endif; ?>
-          <?php if ($mission): print '<div id="mission">'. $mission .'</div>'; endif; ?>
-          <?php if ($title): print '<h1>'. $title .'</h1>'; endif; ?>
-          <?php if ($tabs2): print '<div id="tabs2"><ul class="tabs secondary">'. $tabs2 .'</ul></div>'; endif; ?>
-          <div class="clear-block"></div>
-          <?php if ($show_messages && $messages): print $messages; endif; ?>
-          <?php if (arg(0) == 'node' || arg(0) == 'taxonomy' || arg(0) == 'image') :
-global $user;
-$user_roles = array_keys($user->roles);
-if (in_array(6, $user_roles) || in_array(12, $user_roles)) {
-  print '<span class="admininfo dontprint"><strong>Link hierheen:</strong> internal:' . $_GET['q'] . ' - http://www.thirdwing.nl/' . $_GET['q'] . '</span>';
-}
-endif; ?><?php print $help; ?>
-          <div class="clear-block">
-            <?php print $content ?>
-          </div>
-          <?php print $feed_icons ?>
-      </div></div></div></div> <!-- /.left-corner, /.right-corner, /#squeeze, /#center -->
+  </div> <!-- /container -->
 
-      <?php if ($right): ?>
-        <div id="sidebar-right" class="sidebar">
-          <?php if (!$left && $search_box): ?><div class="block block-theme"><?php print $search_box ?></div><?php endif; ?>
-          <?php print $right ?>
-        </div>
-      <?php endif; ?>
+  <div id="footer"><div class="container clear-block"><?php print $last_updated ?><?php print $footer_message ?></div></div>
 
-          <div id="footer"><?php print $last_updated ?><?php print $footer_message ?></div>
-    </div> <!-- /container -->
-  </div><!-- /layout -->
+  <div id="to-top"><a href="#top" title="Terug naar boven"></a></div>
 
-  <?php print $closure ?>
-  </body>
-</html>
+</div><!-- /wrapper -->
+
+<?php print $closure ?>
+
+</body>
+
+<html>
